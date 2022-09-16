c74c3e576e9cafbe6a5fb9ffee0141c0a6151ec9
Yinandi <yinandi.tech@gmail.com>
2022-09-16 19:34:43 -0400

    Update app.js buttons and index.html title

--- app.js
+++ app.js
@@ -5,76 +5,77 @@ const btnNext = document.getElementById('btnNext');
 const btnLast = document.getElementById('btnLast');
 
 const displayButtons= () => {
-    if (page === 1){
-        btnFirst.style.visibility = 'hidden';
-        btnBack.style.visibility = 'hidden';
-        btnNext.style.visibility = 'visible';
-        btnLast.style.visibility = 'visible';
-    }
-    if (page > 1 & page < 500){
-        btnFirst.style.visibility = 'visible';
-        btnBack.style.visibility = 'visible';
-        btnNext.style.visibility = 'visible';
-        btnLast.style.visibility = 'visible';
-    }  
-    if (page === 500){
-        btnFirst.style.visibility = 'visible';
-        btnBack.style.visibility = 'visible';
-        btnNext.style.visibility = 'hidden';
-        btnLast.style.visibility = 'hidden';
-    }
+  if (page === 1){
+    btnFirst.style.visibility = 'hidden';
+    btnBack.style.visibility = 'hidden';
+    btnNext.style.visibility = 'visible';
+    btnLast.style.visibility = 'visible';
+  }
+  else if (page > 1 & page < 500){
+    btnFirst.style.visibility = 'visible';
+    btnBack.style.visibility = 'visible';
+    btnNext.style.visibility = 'visible';
+    btnLast.style.visibility = 'visible';
+  }  
+  else if (page === 500){
+    btnFirst.style.visibility = 'visible';
+    btnBack.style.visibility = 'visible';
+    btnNext.style.visibility = 'hidden';
+    btnLast.style.visibility = 'hidden';
+  }
 }
 
 btnFirst.addEventListener('click', ()=>{
-    if(page > 1){
-        page = 1;
-        loadMovies();
-    }
+  if(page > 1){
+    page = 1;
+    loadMovies();
+  }
 
 }) 
 btnBack.addEventListener('click', ()=>{
-    if (page > 1){
-        page -=1;
-        loadMovies();
-    }
+  if (page > 1){
+    page -=1;
+    loadMovies();
+  }
 })
 btnNext.addEventListener('click', ()=>{
-    if (page < 500){
-        page +=1;
-        loadMovies();
-    }
-})
+  if (page < 500){
+    page +=1;
+    loadMovies();
+  }
+}) 
 btnLast.addEventListener('click', ()=>{
-    if(page != 500){
-        page = 500;
-        loadMovies();
-    }
+  if(page != 500){
+    page = 500;
+    loadMovies();
+  }
 })  
 
 const loadMovies = async() => {
-    try{
-        const response = await axios.get('https://api.themoviedb.org/3/movie/popular?',{
-            params:{
-                page: page
-            },
-            headers: {
-                'Authorization':  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NmVhYTZkZDRhMGM0OGI3YjQ0NzFjZmI2OWI4OGMzMiIsInN1YiI6IjYzMGZhNGY2MTUxMWFhMDA5MjRjNjM4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1LlR8Gnis4tQc-G37zF7iTcX5zpdSC6bu4OqR-F9nhE'
-            }
-        });
-        if (response.status === 200){
-            let movies = '';
-            response.data.results.forEach(movie =>{
-                movies += `
-                <div class="movie">
-                    <img class="poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
-                    <h3 class="title">${movie.title}</h3>
-                </div>`
-           });
-           document.getElementById('container').innerHTML = movies;
-           displayButtons();
-        }
-    }catch(error){
-        console.log(error);
+  try{
+    const response = await axios.get('https://api.themoviedb.org/3/movie/popular?',{
+      params:{
+        page: page
+      },
+      headers: {
+        'Authorization':  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NmVhYTZkZDRhMGM0OGI3YjQ0NzFjZmI2OWI4OGMzMiIsInN1YiI6IjYzMGZhNGY2MTUxMWFhMDA5MjRjNjM4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1LlR8Gnis4tQc-G37zF7iTcX5zpdSC6bu4OqR-F9nhE'
+      }
+    });
+    if(response.status === 200){
+      let movies = '';
+      response.data.results.forEach(movie =>{
+          movies += `
+          <div class="movie">
+            <img class="poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+            <h3 class="title">${movie.title}</h3>
+          </div>`
+      });
+      document.getElementById('container').innerHTML = movies;
+      displayButtons();
     }
+  }catch(error){
+    console.log(error);
+  }
 }
+
 loadMovies();
